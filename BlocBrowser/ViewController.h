7ab960637a33274c45932e81e65d039e91fcc789
Matthew Yang <imatthewyang@gmail.com>
2014-12-22 08:18:37 -0800

    Initial Commit

---
+++ BlocBrowser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocBrowser
+//
+//  Created by Matthew Yang on 12/22/14.
+//  Copyright (c) 2014 Tmrw, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BlocBrowser/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Matthew Yang on 12/22/14.
+//  Copyright (c) 2014 Tmrw, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
